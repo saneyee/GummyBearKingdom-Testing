7b64615ce2097f01deaba1e9d7feb722564e9fed
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-06 18:10:38 -0800

    adds GummyBearDbContext.cs

---
+++ GummyBearKingdom/Models/GummyBearKingdomDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummyBearKingdom.Models
+{
+    public class GummyBearKingdomDbContext : DbContext
+    {
+        public GummyBearKingdomDbContext()
+        {
+        }
+
+
+        public DbSet<Property> Properties { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=GummyBearKingdom;uid=root;pwd=root;");
+        }
+
+        public GummyBearKingdomDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
