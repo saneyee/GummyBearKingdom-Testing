160433e7ce83e2bc2b964cc4ddc13283eddf8103
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-07 15:26:13 -0800

    adds edit feature

--- GummyBearKingdom/Controllers/PropertiesController.cs
+++ GummyBearKingdom/Controllers/PropertiesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummyBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -37,6 +38,22 @@ namespace GummyBearKingdom.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProperty = db.Properties.FirstOrDefault(properties => properties.PropertyId == id);
+
+            return View(thisProperty);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Property property)
+        {
+            db.Entry(property).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
 
 
