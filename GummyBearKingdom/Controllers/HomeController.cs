3c9b469485c9f6757014bfaa4ee85e5464ca2349
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-07 12:38:08 -0800

    updates the homecontroller

--- GummyBearKingdom/Controllers/HomeController.cs
+++ GummyBearKingdom/Controllers/HomeController.cs
@@ -8,26 +8,8 @@ namespace GummyBearKingdom.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
         {
             return View();
         }
