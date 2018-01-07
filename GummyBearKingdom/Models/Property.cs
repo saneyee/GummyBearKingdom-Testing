3da66b7ec44b7ed55cd0736bbd6b884092cc435d
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-06 18:01:48 -0800

    it adds the models folder with Property.cs file

---
+++ GummyBearKingdom/Models/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummyBearKingdom.Models
+{
+    
+    public class Property
+    {
+        [Key]
+        public int PropertyId { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public string Description { get; set; }
+
+    }
+
+   
+}
