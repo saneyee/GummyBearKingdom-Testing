de5934d831433ac48d8d5594a3fd4b375ce4e4d3
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 20:43:16 -0800

    adds a testdbcontext file for the creation of test database

---
+++ GummyBearKingdom.Tests/Models/TestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GummyBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GummyBearKingdom.Tests.Models
+{
+    public class TestDbContext : GummyBearKingdomDbContext
+    {
+        public override DbSet<Property> Properties { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;port=8889;database=gummybearkingdom_test;uid=root;pwd=root;");
+        }
+    }
+}
