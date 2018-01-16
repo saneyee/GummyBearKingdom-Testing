ae561c74d2a2d917c4a2d267ac409e33a0d43558
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 19:50:35 -0800

    creates the mock database set for review controller tests

--- GummyBearKingdom.Tests/ControllerTests/ReviewsControllerTests.cs
+++ GummyBearKingdom.Tests/ControllerTests/ReviewsControllerTests.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GummyBearKingdom.Controllers;
+using GummyBearKingdom.Models;
+using GummyBearKingdom.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+
 namespace GummyBearKingdom.Tests.ControllerTests
 {
+    [TestClass]
     public class ReviewsControllerTests
     {
-        public ReviewsControllerTests()
+        Mock<IReviewRepository> mock = new Mock<IReviewRepository>();
+
+        private void DbSetup()
         {
+            mock.Setup(m => m.Reviews).Returns(new Review[]
+            {
+                new Review {ReviewId = 1, Author= "Soma", Content = "Good", Rating = 2},
+                new Review {ReviewId = 2, Author= "Berry", Content = "Bad", Rating = 5},
+                new Review {ReviewId = 3, Author= "Chainy", Content = "Costly", Rating = 3},
+            }.AsQueryable());
         }
+
+
+
+
+
     }
 }
