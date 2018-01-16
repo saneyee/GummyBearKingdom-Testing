c7637748a7912907da1e434e19ce03aea57a1d02
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 16:09:54 -0800

    updates the property model

--- GummyBearKingdom/Models/Property.cs
+++ GummyBearKingdom/Models/Property.cs
@@ -17,6 +17,32 @@ namespace GummyBearKingdom.Models
         public int ReviewId { get; set; }
         public virtual Review Review { get; set; }
 
+
+
+        public override bool Equals(System.Object otherProperty)
+        {
+            if (!(otherProperty is Property))
+            {
+                return false;
+            }
+            else
+            {
+                Property newProperty = (Property)otherProperty;
+                bool EqualId = this.PropertyId.Equals(newProperty.PropertyId);
+                bool EqualName = this.Name.Equals(newProperty.Name);
+                bool EqualCost = this.Cost.Equals(newProperty.Cost);
+                bool EqualDescription = this.Description.Equals(newProperty.Description);
+                bool EqualReviewId = this.ReviewId.Equals(newProperty.ReviewId);
+
+
+                return (EqualId && EqualName && EqualCost && EqualDescription && EqualReviewId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.PropertyId.GetHashCode();
+        }
     }
 
    
