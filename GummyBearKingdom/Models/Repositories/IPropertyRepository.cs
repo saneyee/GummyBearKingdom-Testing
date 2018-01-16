063443701f9d072fc926887b1a5b29532be884bf
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 17:20:04 -0800

    adds interfaces to the project containing method signatures for both Property n review

--- GummyBearKingdom/Models/Repositories/IPropertyRepository.cs
+++ GummyBearKingdom/Models/Repositories/IPropertyRepository.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Linq;
+
 namespace GummyBearKingdom.Models.Repositories
 {
     public interface IPropertyRepository
     {
+        IQueryable<Property> Properties { get; }
+        Property Save(Property property);
+        Property Edit(Property property);
+        void Remove(Property property);
     }
 }
--- GummyBearKingdom/Models/Repositories/IReviewRepository.cs
+++ GummyBearKingdom/Models/Repositories/IReviewRepository.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Linq;
+
 namespace GummyBearKingdom.Models.Repositories
 {
     public interface IReviewRepository
     {
+        IQueryable<Review> Reviews { get; }
+        Review Save(Review review);
+        Review Edit(Review review);
+        void Remove(Review review);
     }
 }
