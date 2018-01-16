2d1246c9fd171aa77cdd7deec849bcc0eb675ef7
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 19:58:46 -0800

    all the review route tests are passing

--- GummyBearKingdom.Tests/ControllerTests/ReviewsControllerTests.cs
+++ GummyBearKingdom.Tests/ControllerTests/ReviewsControllerTests.cs
@@ -26,9 +26,97 @@ namespace GummyBearKingdom.Tests.ControllerTests
             }.AsQueryable());
         }
 
+        [TestMethod]
+        public void Mock_GetViewResultIndex_ActionResult() // Confirms route returns view
+        {
+            //Arrange
+            DbSetup();
+            ReviewsController controller = new ReviewsController(mock.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Mock_IndexContainsModelData_List() // Confirms model as list of reviews
+        {
+            // Arrange
+            DbSetup();
+            ViewResult indexView = new ReviewsController(mock.Object).Index() as ViewResult;
+
+            // Act
+            var result = indexView.ViewData.Model;
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(List<Review>));
+        }
+
+        [TestMethod]
+        public void Mock_IndexModelContainsReviews_Collection() // Confirms presence of known entry
+        {
+            // Arrange
+            DbSetup();
+            ReviewsController controller = new ReviewsController(mock.Object);
+            Review testReview = new Review();
+            testReview.Author = "Soma";
+            testReview.Content = "Good";
+            testReview.Rating = 2;
+            testReview.ReviewId = 1;
+
+            // Act
+            ViewResult indexView = controller.Index() as ViewResult;
+            List<Review> collection = indexView.ViewData.Model as List<Review>;
+
+            // Assert
+            CollectionAssert.Contains(collection, testReview);
+        }
+
+        [TestMethod]
+        public void Mock_PostViewResultCreate_ViewResult()
+        {
+            // Arrange
+            Review testReview = new Review
+            {
+                ReviewId = 4,
+                Author = "Darwin"
+            };
+
+            DbSetup();
+            ReviewsController controller = new ReviewsController(mock.Object);
+
+            // Act
+            var resultView = controller.Create(testReview) as ViewResult;
 
 
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
 
+        }
+
+        [TestMethod]
+        public void Mock_GetDetails_ReturnsView()
+        {
+            // Arrange
+            Review testReview = new Review
+            {
+                ReviewId = 4,
+                Author = "Darwin"
+            };
+
+            DbSetup();
+            ReviewsController controller = new ReviewsController(mock.Object);
+
+            // Act
+            var resultView = controller.Details(testReview.ReviewId) as ViewResult;
+            var model = resultView.ViewData.Model as Review;
+
+            // Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
+            Assert.IsInstanceOfType(model, typeof(Review));
+        }
 
     }
 }
