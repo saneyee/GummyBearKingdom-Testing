8c3a180d587d6863278c2905b334343924191147
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 17:00:07 -0800

    model tests for review author passes

--- GummyBearKingdom.Tests/ModelTests/ReviewTests.cs
+++ GummyBearKingdom.Tests/ModelTests/ReviewTests.cs
@@ -1,10 +1,51 @@
-﻿using System;
-namespace GummyBearKingdom.Tests.ModelTests
+﻿using GummyBearKingdom.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GummyBearKingdom.Tests
 {
-    public class EmptyClass
+    [TestClass]
+    public class ReviewTests
     {
-        public EmptyClass()
+        [TestMethod]
+        public void GetAuthor_ReturnsReviewAuthor_String()
         {
+            //Arrange
+            var review = new Review();
+
+
+            //Act
+            var result = review.Author = "Edd";
+
+            //Assert
+            Assert.AreEqual("Edd", result);
         }
+
+        //[TestMethod]
+        //public void GetCost_ReturnsReviewCost_Int()
+        //{
+        //    //Arrange
+        //    var review = new Review();
+
+
+        //    //Act
+        //    var result = review.Cost = 200;
+
+        //    //Assert
+        //    Assert.AreEqual(200, result);
+        //}
+
+        //[TestMethod]
+        //public void GetDescription_ReturnsReviewDescription_String()
+        //{
+        //    //Arrange
+        //    var review = new Review();
+
+
+        //    //Act
+        //    var result = review.Description = "Used in summer";
+
+        //    //Assert
+        //    Assert.AreEqual("Used in summer", result);
+        //}
     }
 }
