c72c2692275942d9d5566ea7403f469bfcf1912b
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-15 19:17:47 -0800

    adds the mock database data set

--- GummyBearKingdom.Tests/ControllerTests/PropertiesControllerTests.cs
+++ GummyBearKingdom.Tests/ControllerTests/PropertiesControllerTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+
+
+
+
 namespace GummyBearKingdom.Tests.ControllerTests
 {
+    [TestClass]
     public class PropertiesControllerTests
     {
-        public PropertiesControllerTests()
+        Mock<IPropertyRepository> mock = new Mock<IPropertyRepository>();
+
+        private void DbSetup()
         {
+            mock.Setup(m => m.Properties).Returns(new Property[]
+            {
+                new Property {PropertyId = 1, Name= "Sofa", Cost = 200, Description = "Comfortable", ReviewId = 1},
+                new Property {PropertyId = 2, Name= "Bed", Cost = 300, Description = "King", ReviewId = 2},
+                new Property {PropertyId = 3, Name= "Chair", Cost = 400, Description = "Folding", ReviewId = 1},
+            }.AsQueryable());
         }
+        
     }
 }
